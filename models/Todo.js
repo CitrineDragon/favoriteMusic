84f4e24ec0e1e848c6b661735382c96c2893702e
Brian Regehr <brian.regehr@gmail.com>
2022-08-29 14:48:14 -0700

    db issues

---
+++ models/Todo.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+  todoSong: {
+    type: String,
+    required: true,
+  },
+  todoArtist: {
+    type: Boolean,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Todo', TodoSchema);
